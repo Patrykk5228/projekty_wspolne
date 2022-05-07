88da7204e36c1888b886601b162092dbd3ed4e83
Duc Trung Bui <txbuiduc@gmail.com>
2022-05-07 20:52:50 +0200

    Update 2

--- ideabank.py
+++ ideabank.py
@@ -1,22 +1,29 @@
 import sys
-tablica = []
 
-def readfile():
+add_list = []
+delete_list = []
 
+def add_idea():
     with open('plik.txt') as f:
         for line in f:
-            tablica.append(line.split(" "))
-    return tablica
+            add_list.append(line.split(" "))
+    return add_list
+
+def remove_idea():
+    with open('plik.txt') as f:
+        for line in f:
+            for idea in delete_list:
+                line = line.replace(idea, "")
+    return f.write(line)
+        
 
 file_open = open("plik.txt", "a")
 
+idea = sys.argv[2]
 
 if __name__ == "__main__":
-    if sys.argv[1] == "add":
-        file_open.write("add\n")
+    if sys.argv[1] == "add" and len(sys.argv[2]) > 0:
+        file_open.write(idea + "\n")
     elif sys.argv[1] == "delete":
-        file_open.write("delete\n")
-
-
-
-
+        remove_idea("delete\n")
+delete_list = []
