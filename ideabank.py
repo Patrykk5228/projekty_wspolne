aba3b7329f800af2e2bd703787409a0d70677877
Duc Trung Bui <txbuiduc@gmail.com>
2022-05-08 14:11:59 +0200

    Update ideabank.py

--- ideabank.py
+++ ideabank.py
@@ -1,29 +1,62 @@
+import readline
 import sys
+import os
 
-add_list = []
-delete_list = []
+def is_string(userInput):
+    if userInput.isdigit() is False:
+        return True
+    return False
 
-def add_idea():
-    with open('plik.txt') as f:
-        for line in f:
-            add_list.append(line.split(" "))
-    return add_list
+def load_idea():
+    with open('File.txt') as f:
+        line = f.readline()
+        load_list.append(line)
+    return load_list
+
+def save_idea():
+    file = open("File.txt","w")
+    file.write(idea)
+    file.close()
+        
 
 def remove_idea():
-    with open('plik.txt') as f:
+    with open('File.txt') as f:
         for line in f:
             for idea in delete_list:
                 line = line.replace(idea, "")
-    return f.write(line)
-        
+                return f.write(line)
+    
+def ask_for_idea(force_valid_input):
+    while True:
+        idea = input("\nWhat is your new idea?")
+        isString = is_string(idea)
+        if isString is False:
+            if not force_valid_input:
+                return None
+            print("Not a correct sentence.")
+        else:
+            return idea
 
-file_open = open("plik.txt", "a")
-
-idea = sys.argv[2]
-
+load_list = []
+delete_list = []
+load_idea()
+while True:
+    try:
+        idea = ask_for_idea(True)
+        load_list.append(idea)
+        for i, item in enumerate(load_list,1):
+            print(i, '. ' + item, sep='',end='' + "\n")
+        save_idea()
+    except KeyboardInterrupt:
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+"""
 if __name__ == "__main__":
+    idea = sys.argv[2]
     if sys.argv[1] == "add" and len(sys.argv[2]) > 0:
         file_open.write(idea + "\n")
-    elif sys.argv[1] == "delete":
-        remove_idea("delete\n")
-delete_list = []
+    elif sys.argv[1] == "delete" and len(sys.argv[2]) > 0:
+        remove_idea()
+"""
